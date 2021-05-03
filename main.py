141c81fcdb20e0e2020ce12020b7dd85a33b80a5
Ed Stubbs <ejds2@cam.ac.uk>
2021-05-03 18:26:58 +0100

    functionalised simulation to run in serial with different settings

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from datetime import datetime
 from simulation_0_0 import simulate
 
-if __name__ == '__main__':
+
+def run(param):
     start = datetime.now()
     now = start.strftime("%d/%m/%Y %H:%M:%S")
     print(' =============================================== \n',
@@ -17,7 +18,7 @@ if __name__ == '__main__':
              # manufacturer settings
              capacity_root_coeff=4.0, speed_of_build=0.2, time_to_build=15.0,
              # regulator settings
-             notice_period=24, fraction=0.1, start_levy=1.5, ratio_jump=0.5, wait_time=48, compliance_threshold=0.5,
+             notice_period=24, fraction=0.1, start_levy=param, ratio_jump=0.5, wait_time=48, compliance_threshold=0.5,
              decade_jump=0.5)
 
     end = datetime.now()
@@ -27,3 +28,9 @@ if __name__ == '__main__':
           now, '\n',
           '==================== \n',
           )
+    return
+
+
+if __name__ == '__main__':
+    for x in [0.2, 0.4, 0.6, 0.8, 1.0, 1.5, 2.0]:
+        run(x)
